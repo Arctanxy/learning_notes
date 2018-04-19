2be2479bc45186751a5cbec91c435d09f5388d62
Dai Liang <luobowamei@126.com>
2018-04-19 22:27:54 +0800

    pandas

--- project/kaggle/iMaterialist Challenge/download.py
+++ project/kaggle/iMaterialist Challenge/download.py
@@ -7,7 +7,7 @@ import time
 import os
 
 FOLDER = 'H:/learning_notes/project/kaggle/iMaterialist Challenge/'
-
+DOWNLOAD_PATH = 'H:/google_image/'
 
 def download_data(is_type = 'train'):
     time.sleep(0.5)
@@ -22,39 +22,39 @@ def download_data(is_type = 'train'):
         #images.to_csv(FOLDER + is_type + '_data.csv',index = False)#已经保存过了
         print('total num:%d' % images.shape[0])
         for i,row in images.iterrows():
-            if os.path.exists(FOLDER + is_type + '_image/' + row['name']):#如果这个文件已经存在了，就不需要重复下载了
+            if os.path.exists(DOWNLOAD_PATH + is_type + '_image/' + row['name']):#如果这个文件已经存在了，就不需要重复下载了
                 continue       
             if i%10 == 0:
                 print('train %d' % i)
             try:
-                urllib.request.urlretrieve(row['url'],filename=FOLDER + is_type + '_image/' + row['name'])
-            except:
-                pass
+                urllib.request.urlretrieve(row['url'],filename=DOWNLOAD_PATH + is_type + '_image/' + row['name'])
+            except Exception as e:
+                print(e)
     elif is_type == 'validation':
         images['label'] = images['annotations'].apply(lambda x:x['label_id'])#图片分类标签
         del images['annotations']
         #images.to_csv(FOLDER + is_type + '_data.csv',index = False)#已经保存过了
         print('total num:%d' % images.shape[0])
         for i,row in images.iterrows():
-            if os.path.exists(FOLDER + is_type + '_image/' + row['name']):
+            if os.path.exists(DOWNLOAD_PATH + is_type + '_image/' + row['name']):
                 continue 
             if i%10 == 0:
                 print('validation %d' % i)
             try:
-                urllib.request.urlretrieve(row['url'],filename=FOLDER + is_type + '_image/' + row['name'])
-            except:
-                pass
+                urllib.request.urlretrieve(row['url'],filename=DOWNLOAD_PATH + is_type + '_image/' + row['name'])
+            except Exception as e:
+                print(e)
     else:
         print('total num:%d' % images.shape[0])
         for i,row in images.iterrows():
-            if os.path.exists(FOLDER + is_type + '_image/' + row['name']):
+            if os.path.exists(DOWNLOAD_PATH + is_type + '_image/' + row['name']):
                 continue 
             if i%10 == 0:
                 print('test %d' % i)
             try:
-                urllib.request.urlretrieve(row['url'],filename=FOLDER + is_type + '_image/' + row['name'])
-            except:
-                pass
+                urllib.request.urlretrieve(row['url'],filename=DOWNLOAD_PATH + is_type + '_image/' + row['name'])
+            except Exception as e:
+                print(e)
     
 if __name__ == "__main__":
     threads = []
