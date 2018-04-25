787869b3938207460a85ff20b13cfcc82ed3277d
Dai Liang <luobowamei@126.com>
2018-04-25 21:35:27 +0800

    shapely

--- project/kaggle/iMaterialist Challenge/model.py
+++ project/kaggle/iMaterialist Challenge/model.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np 
 import os
 import glob as gb # Python下的文件操作模块
+import pandas as pd 
 
 BATCH_SIZE = 100
 PATH = "H:/google_image/"
@@ -17,28 +18,42 @@ def get_data(path = PATH,is_type = 'train',batch_size = BATCH_SIZE):
     train_path = train_jpg_path + train_png_path
     train_images = []
     count = 0
-    print("^^Reading Images^^")
-    print(train_path)
-    for path in train_path:
-        print(count)
-        if count == BATCH_SIZE - 1:
-            break
+
+    # 随机选择BATCH_SIZE个图片
+    index = [int(len(train_path) * p) for p in np.random.random(100)]
+    choosen_path = [train_path[i] for i in index]
+    
+    # 添加标签
+    label_list = []
+    label_dict = get_label("train")
+    
+    for path in choosen_path:
         img = cv2.imread(path)
         # 有些图片损坏了，无法进行resize或者imshow，但是可以使用imread
         try:
             img = cv2.resize(img,(400,400))
             train_images.append(img)
+            print(type(img))
             count += 1
             # cv2.imshow('img',img)
         except Exception as e:
             print(path)
             print(e)
         # cv2.waitKey(1)
-    
-    return train_images
+        img_name = path.split('\\')[-1]
+        label = label_dict[img_name]
+        label_list.append(label)
+        
+    return train_images,label_list
 
-def get_label(path,is_type):
-    label_df = 
+def get_label(is_type):
+    '''
+    返回文件名与label组成的字典，方便对照
+    '''
+    label_df = pd.read_csv("H:/learning_notes/project/kaggle/iMaterialist Challenge/" + is_type + "_data.csv")
+    name = label_df['name']
+    label = label_df['label']
+    return dict(zip(name,label))
 
 if __name__ == "__main__":
-    print(train_data())
+    get_data()
